14d3173feaeb7bda8bc0b25572f09953a65900f3
sirily11 <32106111+sirily11@users.noreply.github.com>
2026-01-21 12:32:58 +0800

    fix: sidebar breadcrumb

--- frontend/components/ui/breadcrumb.tsx
+++ frontend/components/ui/breadcrumb.tsx
@@ -20,7 +20,7 @@ function BreadcrumbList({ className, ...props }: React.ComponentProps<"ol">) {
     <ol
       data-slot="breadcrumb-list"
       className={cn(
-        "text-muted-foreground gap-1.5 text-sm sm:gap-2.5 flex flex-wrap items-center break-words",
+        "text-muted-foreground gap-1.5 text-sm sm:gap-2.5 flex items-center",
         className
       )}
       {...props}
